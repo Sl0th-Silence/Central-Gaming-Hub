c9a9cf47524f52c56c957e1516a77ae1364bcf89
coolsmitty9000 <sawyersmith555@gmail.com>
2025-10-30 10:25:43 -0400

    Got Electron working, albeit only with html files.
    Stylesheet is also created and linked, but that will be worked on later when data is obtained and a format gets established.

---
+++ Electron/main.js
@@ -0,0 +1,23 @@
+/* USE: "npm run start" TO LAUNCH THE APP ON PC */
+
+
+const { app, BrowserWindow } = require('electron');
+
+//creates a electron window
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 800
+  })
+
+  win.loadFile('index.html')
+}
+
+//renders a window when the program starts
+app.whenReady().then(() => {
+  createWindow()
+})
+
+app.on("window-all-closed", () => {
+    app.quit();
+})
