5b0173afb48127d2651c56591abd88c809dfdf1a
Jay Mills <jay_michael_mills@live.com>
2025-11-07 09:08:17 -0500

    Cleanup dir

--- Electron/main.js
+++ Electron/main.js
@@ -8,6 +8,23 @@ const fileReader = require("fs");
 //the default steam directory, this will be changable at a later point (probably)
 const steamDIR = "C:/Program Files (x86)/Steam/steamapps/common";
 
+
+//Checking for logos -Jay
+const steamImageDIR = "C:/Program Files (x86)/Steam/appcache/librarycache/"
+const steamImageArr = fileReader.readdirSync(steamImageDIR);
+allImages = []
+
+//For each folder, add the name at the end of the url
+steamImageArr.forEach(item => {
+    image = (steamImageDIR + item + "/header");
+    if(image)z
+    {
+      //Add to array
+      allImages.push(image);
+    }
+});
+
+
 const gameArr = fileReader.readdirSync(steamDIR);
 
 //creates a electron window
